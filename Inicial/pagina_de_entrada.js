789c48879bd7f4ce2bb67c2ec380fa7791bc0bb8
Riversson Paulo <150279469+riverssonpaulo@users.noreply.github.com>
2023-12-23 18:31:39 -0300

    Add files via upload

--- Inicial/pagina_de_entrada.js
+++ Inicial/pagina_de_entrada.js
@@ -17,3 +17,34 @@ const intervalo = setInterval(proximoslide, 3000);
 const carrosel = document.querySelector('.carrosel');
 carrosel.addEventListener('mouseenter', () => clearInterval(intervalo));
 carrosel.addEventListener('mouseleave', () => intervalo = setInterval(proximoslide, 3000));
+
+
+var carrinho = document.getElementById('carrinho_pag');
+carrinho.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/carrinho/carrinho.html"><i class="bx bx-cart-alt bx-md bx-tada"></i></a>'
+
+var carrinho_responsivo = document.getElementById('carrinho_pag_responsivo');
+carrinho_responsivo.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/carrinho/carrinho.html"><i class="bx bx-cart-alt bx-md bx-tada"></i></a>'
+
+var login = document.getElementById('login_pag');
+login.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/login/pagina_de_login.html"><i class="bx bx-user bx-md "></i></a>'
+
+var login_responsivo = document.getElementById('login_pag_responsivo');
+login_responsivo.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/login/pagina_de_login.html"> <i class="bx bx-user bx-md "></i></a>'
+
+var whey = document.getElementById('whey');
+whey.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/whey_protein/whey_protein.html"><button class="botoes_menu" id="whey"> Whey Protein </button></a>'
+
+var creatina = document.getElementById('creatina');
+creatina.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/creatinas/creatinas.html"><button class="botoes_menu" id="creatina"> Creatina </button></a>'
+
+var roupas = document.getElementById('roupas');
+roupas.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/roupas/roupas.html"><button class="botoes_menu" id="roupas"> Roupas </button> </a>'
+
+var acessorios = document.getElementById('acessorios');
+acessorios.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/acessorios/acessorios.html"><button class="botoes_menu" id="acessorios"> Acessórios </button></a>'
+
+var dashboard = document.getElementById('dashboard');
+dashboard.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/acessorios/acessorios.html"><button class="botoes_menu" id="dashboard"> Dashboard </button></a>'
+
+
+
