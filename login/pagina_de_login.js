d88fef7af6f58263664834781143d040641ba1d4
Mateus Lucena <150374285+Mathewxs21@users.noreply.github.com>
2023-12-23 15:44:11 -0300

    Add files via upload

---
+++ login/pagina_de_login.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('#form');
+const campos = document.querySelectorAll('.required');
+const spans = document.querySelectorAll('.span-required')
+
+function setError(index){
+    campos[index].style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
+}
+
+function removeError(index){
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
+}
+
+function nameValidate() {
+    if(campos[0].value.length < 3) {
+        setError(0);
+    }
+
+    else {
+        removeError(0);
+    }
+
+}
+
+function maisPasswordValidate() {
+    if(campos[1].value.length < 8) {
+        setError(1);
+    }
+    else {
+        removeError(1);
+    }
+}
