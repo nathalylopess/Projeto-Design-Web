2831bea13bf9972f83dadad3dd1d57b99569a2ab
Riversson Paulo <150279469+riverssonpaulo@users.noreply.github.com>
2023-12-26 13:45:41 -0300

    Add files via upload

---
+++ creatinas/creatinas.js
@@ -0,0 +1,5 @@
+var carrinho = document.getElementById('carrinho_pag');
+carrinho.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/carrinho/carrinho.html"><i class="bx bx-cart-alt bx-md bx-tada"></i></a>'
+
+var login = document.getElementById('login_pag');
+login.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/login/pagina_de_login.html"><i class="bx bx-user bx-md "></i></a>'
