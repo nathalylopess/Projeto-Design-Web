579a6f21b1212fb9006cdb39b654eddd5e633f9a
Riversson Paulo <150279469+riverssonpaulo@users.noreply.github.com>
2023-12-28 14:00:17 -0300

    Add files via upload

--- Inicial/pagina_de_entrada.js
+++ Inicial/pagina_de_entrada.js
@@ -18,33 +18,43 @@ const carrosel = document.querySelector('.carrosel');
 carrosel.addEventListener('mouseenter', () => clearInterval(intervalo));
 carrosel.addEventListener('mouseleave', () => intervalo = setInterval(proximoslide, 3000));
 
-
 var carrinho = document.getElementById('carrinho_pag');
-carrinho.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/carrinho/carrinho.html"><i class="bx bx-cart-alt bx-md bx-tada"></i></a>'
+carrinho.addEventListener("click", function(){
+    window.location.href =
+})
 
 var carrinho_responsivo = document.getElementById('carrinho_pag_responsivo');
-carrinho_responsivo.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/carrinho/carrinho.html"><i class="bx bx-cart-alt bx-md bx-tada"></i></a>'
+carrinho_responsivo.addEventListener("click", function(){
+    window.location.href =
+})
 
 var login = document.getElementById('login_pag');
-login.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/login/pagina_de_login.html"><i class="bx bx-user bx-md "></i></a>'
+login.addEventListener("click", function(){
+  window.location.href =
+})
 
 var login_responsivo = document.getElementById('login_pag_responsivo');
-login_responsivo.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/login/pagina_de_login.html"> <i class="bx bx-user bx-md "></i></a>'
+login_responsivo.addEventListener("click", function(){
+  window.location.href =
+})
 
 var whey = document.getElementById('whey');
-whey.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/whey_protein/whey_protein.html"><button class="botoes_menu" id="whey"> Whey Protein </button></a>'
+whey.addEventListener("click", function(){
+  window.location.href =
+})
 
 var creatina = document.getElementById('creatina');
-creatina.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/creatinas/creatinas.html"><button class="botoes_menu" id="creatina"> Creatina </button></a>'
-
-var roupas = document.getElementById('roupas');
-roupas.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/roupas/roupas.html"><button class="botoes_menu" id="roupas"> Roupas </button> </a>'
+creatina.addEventListener("click", function(){
+  window.location.href =
+})
 
 var acessorios = document.getElementById('acessorios');
-acessorios.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/acessorios/acessorios.html"><button class="botoes_menu" id="acessorios"> Acessórios </button></a>'
+acessorios.addEventListener("click", function(){
+  window.location.href =
+})
 
 var dashboard = document.getElementById('dashboard');
-dashboard.innerHTML = '<a href="file:///C:/Users/river/OneDrive/%C3%81rea%20de%20Trabalho/Infoweb/Design%20Web/projeto%20design/acessorios/acessorios.html"><button class="botoes_menu" id="dashboard"> Dashboard </button></a>'
-
-
+dashboard.addEventListener("click", function(){
+  window.location.href =
+})
 
